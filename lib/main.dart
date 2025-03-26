938536c8a6f7da932b544565ce7d12488be31042
ahmed adel <ahmedadel123422@gmail.com>
2025-03-27 00:02:36 +0200

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,12 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, prefer_const_constructors_in_immutables
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(MainApp());
 }
 
 class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
+  MainApp({super.key});
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
